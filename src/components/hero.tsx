e3f4714568725646aaefa9e482e70d21f960afe2
João Gabriel Tonaco User <joao.tonaco@proton.me>
2024-09-06 14:40:14 -0300

    refactor: redesign hero and write copy

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,15 @@
 import { IconArrowDown } from "@tabler/icons-react";
+import Link from "next/link";
 
 import { cn } from "@/lib/utils";
-import { Button } from "./ui/button";
+import { buttonVariants } from "./ui/button";
 import { GridPattern } from "./ui/grid-pattern";
 
 export function Hero() {
 	return (
 		<section
 			id="hero"
-			className="relative overflow-hidden bg-gradient-to-r from-zinc-950 to-zinc-950/80 px-8 py-48 text-zinc-400"
+			className="relative bg-gradient-to-b from-transparent via-primary/10 to-transparent px-8 py-48"
 		>
 			<div className="mx-auto w-full max-w-6xl text-center">
 				<div className="flex flex-col items-center gap-6 *:z-10">
@@ -16,15 +17,17 @@ export function Hero() {
 						Solufy
 					</h1>
 
-					<p className="max-w-4xl pl-0.5">
-						Unleash your software's full potential with our expert development
-						services. From custom web applications to scalable cloud solutions,
-						we deliver innovative technology that drives your business forward.
+					<p className="max-w-4xl pl-0.5 text-base/relaxed text-zinc-600">
+						Libere todo o seu potencial com nossos serviços de desenvolvimento
+						especializado. De aplicativos web personalizados a soluções de
+						sistemas escaláveis, entregamos tecnologia inovadora que impulsiona
+						seu negócio.
 					</p>
 
-					<Button variant="secondary">
-						Saiba mais <IconArrowDown className="ml-2 size-4 text-foreground" />
-					</Button>
+					<Link href="#features" className={buttonVariants()}>
+						Saiba mais
+						<IconArrowDown className="ml-2 size-4 text-primary-foreground" />
+					</Link>
 				</div>
 
 				<GridPattern
@@ -32,8 +35,9 @@ export function Hero() {
 					maxOpacity={0.1}
 					duration={3}
 					className={cn(
-						"[mask-image:radial-gradient(800px_circle_at_center,white,transparent)]",
-						"inset-x-0 inset-y-[-50%] h-[200%] opacity-50",
+						"[mask-image:linear-gradient(transparent_25%,white,transparent_75%)]",
+						"inset-x-0 inset-y-[-50%] h-[200%] opacity-70",
+						"fill-primary/20 stroke-primary/20",
 					)}
 				/>
 			</div>
