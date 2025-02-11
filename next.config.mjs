9bf5591299accd644dff2df7eb2f362a73ee9b33
João Gabriel <joao.tonaco@proton.me>
2025-02-11 02:35:34 -0300

    ci: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "standalone",
+	eslint: { ignoreDuringBuilds: true },
+	devIndicators: { appIsrStatus: false },
+	poweredByHeader: false,
+};
 
 export default nextConfig;
